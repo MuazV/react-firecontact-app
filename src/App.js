3b9fafe21355201fd537bc3f9626238c5d1a9fc6
muaz <muaz.vakitsayan@gmail.com>
2022-07-25 06:45:51 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import Form from "./components/form/Form";
+import Tables from "./components/table/Table";
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Form/>
+      <Tables/>
     </div>
   );
 }
